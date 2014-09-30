6a36e5eda99c932bc3e88d02ec90195a21a1c355
Gant <GantMan@gmail.com>
2014-09-30 15:37:02 -0500

    now lists ppm files and added h2

--- coffee_board.rb
+++ coffee_board.rb
@@ -1,5 +1,15 @@
 class CoffeeBoard < Sinatra::Base
+  SCROLL_FOLDER = "./scroll_files"
+
   get '/' do
+    @image_files = get_all_scrollers(SCROLL_FOLDER) || []
+    p @image_files
     haml :index, :format => :html5, :layout => :simple_layout
+  end
+
+  def get_all_scrollers folder
+    images = %w[ppm PPM]
+    images.map! {|ext| "#{folder}/*.#{ext}" }
+    FileList[*images]
   end
 end
