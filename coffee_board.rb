e965dc39061e248b1461ab818f153a8eff3485d5
Gant <GantMan@gmail.com>
2014-09-30 16:19:52 -0500

    nicified scrollfiles in look and code :sparkles:

--- coffee_board.rb
+++ coffee_board.rb
@@ -2,14 +2,12 @@ class CoffeeBoard < Sinatra::Base
   SCROLL_FOLDER = "./scroll_files"
 
   get '/' do
-    @image_files = get_all_scrollers(SCROLL_FOLDER) || []
-    p @image_files
+    @image_files = get_all_scrollers
     haml :index, :format => :html5, :layout => :simple_layout
   end
 
-  def get_all_scrollers folder
-    images = %w[ppm PPM]
-    images.map! {|ext| "#{folder}/*.#{ext}" }
-    FileList[*images]
+  def get_all_scrollers
+    images = ["#{SCROLL_FOLDER}/*.ppm"]
+    FileList[*images].map {|file| file.match('([^\/]+)\..+$')[0]}
   end
 end
