77c9ba11c52c62aed1aba3de8b5adb8fe074b17d
Gant <GantMan@gmail.com>
2014-10-01 10:54:24 -0500

    PPMs are now links to a working path

--- coffee_board.rb
+++ coffee_board.rb
@@ -6,8 +6,22 @@ class CoffeeBoard < Sinatra::Base
     haml :index, :format => :html5, :layout => :simple_layout
   end
 
+  get '/set_scroll/:file' do
+    # run magical code to change scroller to params[:file]
+    redirect back
+  end
+
   def get_all_scrollers
     images = ["#{SCROLL_FOLDER}/*.ppm"]
     FileList[*images].map {|file| file.match('([^\/]+)\..+$')[0]}
+  end
+
+
+  helpers do
+    def link_to(url,text=url,opts={})
+      attributes = ""
+      opts.each { |key,value| attributes << key.to_s << "=\"" << value << "\" "}
+      "<a href=\"#{url}\" #{attributes}>#{text}</a>"
+    end
   end
 end
