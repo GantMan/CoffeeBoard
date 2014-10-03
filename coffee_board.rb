29bdbf300156fd53c077e9a9b946dd94bdb3a3c4
Gant <GantMan@gmail.com>
2014-10-03 14:48:40 -0400

    simply added sample upload

--- coffee_board.rb
+++ coffee_board.rb
@@ -16,6 +16,21 @@ class CoffeeBoard < Sinatra::Base
     FileList[*images].map {|file| file.match('([^\/]+)\..+$')[0]}
   end
 
+  post '/upload' do
+    unless params[:file] &&
+           (tmpfile = params[:file][:tempfile]) &&
+           (name = params[:file][:filename])
+      @error = "No file selected"
+      return haml(:upload)
+    end
+    STDERR.puts "Uploading file, original name #{name.inspect}"
+    while blk = tmpfile.read(65536)
+      # here you would write it to its final location
+      STDERR.puts blk.inspect
+    end
+    "Upload complete"
+  end
+
 
   helpers do
     def link_to(url,text=url,opts={})
